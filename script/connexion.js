6e201c16ea11a364c4eafe8c7d9c2998729b7157
s <glcsofiane40@gmail.com>
2023-12-07 21:25:43 +0100

    test

--- script/connexion.js
+++ script/connexion.js
@@ -1,3 +1,7 @@
 function connexion() {
-        window.location.href = "../pages/firstpage.html";
+    // Récupère la valeur du champ pseudo
+    var pseudoValue = document.getElementById("pseudo").value;
+    // Vous pouvez également stocker la valeur dans une variable globale pour une utilisation ultérieure
+    window.pseudoVariable = pseudoValue;
+    window.location.href = "../pages/firstpage.html";
 }
