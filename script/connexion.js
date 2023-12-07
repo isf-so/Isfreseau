2b1a3bc3e983d568603960811ed998fee4628ea1
s <glcsofiane40@gmail.com>
2023-12-07 21:40:31 +0100

    1/4

--- script/connexion.js
+++ script/connexion.js
@@ -1,7 +1,7 @@
 function connexion() {
     // Récupère la valeur du champ pseudo
     var pseudoValue = document.getElementById("pseudo").value;
-    // Vous pouvez également stocker la valeur dans une variable globale pour une utilisation ultérieure
+    //stocker la valeur dans une variable globale pour une utilisation ultérieure
     window.pseudoVariable = pseudoValue;
     window.location.href = "../pages/firstpage.html";
 }
